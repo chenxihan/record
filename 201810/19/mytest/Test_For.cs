7ae8c7f88a0d990bb300ded77af3d9fb500496e0
chenxihan <39304431+chenxihan@users.noreply.github.com>
2018-10-19 21:07:01 +0800

    [+]性能分析

---
+++ 201810/19/mytest/Test_For.cs
@@ -0,0 +1,98 @@
+﻿
+using UnityEngine;
+using UnityEngine.Profiling;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Text;
+
+public class Test_For : MonoBehaviour
+{
+    public void Start()
+    {
+        RuntimeProfiler.instance.isRunProfiler = true;
+    }
+    public void StartTest()
+    {
+        int[] arr = new int[20000];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = i;
+        }
+
+        TimeRecord.StartRecord(Application.persistentDataPath + "/Record/test.txt", "foreach");
+        Dictionary<int, int> dic = new Dictionary<int, int>(20000);
+
+        int num = 0;
+        foreach (var item in arr)
+        {
+            dic.Add(num, num++);
+        }
+        TimeRecord.StopRecord("foreach");
+
+        Test test = new Test();
+        test.StartTest();
+
+        //var e = arr.GetEnumerator();
+        //while (e.MoveNext())
+        //{
+        //    dic.Add(num, num++);
+        //}
+
+        GC.Collect();
+        uint startSize = Profiler.GetMonoUsedSize();
+        new List<int>(330);
+        new List<int>(330);
+        new List<int>(330);
+        new List<int>(330);
+        new List<int>(330);
+
+        new List<int>(330);
+        new List<int>(330);
+        //new List<int>(330);
+        //new List<int>(330);
+        //new List<int>(330);
+        new List<int>(246);
+        uint stopSize = Profiler.GetMonoUsedSize();
+        print("stopSize = " + stopSize + ", startSize = " + startSize + " ,增加了：" + (stopSize - startSize));
+
+        GC.Collect();
+        startSize = Profiler.GetMonoUsedSize();
+        new List<int>(6516);
+        stopSize = Profiler.GetMonoUsedSize();
+        print("stopSize = " + stopSize + ", startSize = " + startSize + " ,增加了：" + (stopSize - startSize));
+
+        GC.Collect();
+        startSize = Profiler.GetMonoUsedSize();
+        new List<int>(512);
+        new List<int>(512);
+        new List<int>(512);
+        new List<int>(512);
+        new List<int>(512);
+
+        new List<int>(330);
+        stopSize = Profiler.GetMonoUsedSize();
+        print("stopSize = " + stopSize + ", startSize = " + startSize + " ,增加了：" + (stopSize - startSize));
+
+        print("usedHeapSize : " + (float)Profiler.usedHeapSize / (1024*1024) + " M");
+        print("GetMonoUsedSize : " + (float)Profiler.GetMonoUsedSize() / (1024 * 1024) + " M");
+        print("GetMonoHeapSize : " + (float)Profiler.GetMonoHeapSize() / (1024 * 1024) + " M");
+        print("GetTotalAllocatedMemory : " + (float)Profiler.GetTotalAllocatedMemory() / (1024 * 1024) + " M");
+        print("GetTotalReservedMemory : " + (float)Profiler.GetTotalReservedMemory() / (1024 * 1024) + " M");
+        print("GetTotalUnusedReservedMemory : " + (float)Profiler.GetTotalUnusedReservedMemory() / (1024 * 1024) + " M");
+
+        print(Application.persistentDataPath + "/Record/");
+        TimeRecord.StartRecord(Application.persistentDataPath + "/Record/test.txt", "for_1qw");
+        for (int i = 0; i < 10000000; i++)
+        {
+
+        }
+        TimeRecord.StopRecord("for_1qw");
+        TimeRecord.recordDic.Clear();
+    }
+
+    private void OnDestroy()
+    {
+        //GC.Collect();
+    }
+}
---
+++ 201810/19/mytest/TimeRecord.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+public class TimeRecord
+{
+    public static Dictionary<string, RecordItem> recordDic = new Dictionary<string, RecordItem>();
+    
+    public static void StartRecord(string filePath, string name)
+    {
+        //如果有相同的name,就让报错
+        recordDic.Add(name, new RecordItem(filePath, name, System.DateTime.Now.Ticks));
+    }
+    public static void StopRecord(string name)
+    {
+        RecordItem _item = recordDic[name];
+        _item.stopTime = System.DateTime.Now.Ticks;
+        _item.filePath = _item.filePath.Replace("\\", "/");
+        string folderPath = _item.filePath.Remove(_item.filePath.LastIndexOf("/"));
+
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+        FileStream fs = new FileStream(recordDic[name].filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        byte[] buffer = new byte[20480];//20KB
+        int r = fs.Read(buffer, 0, 20480);
+        string s = Encoding.Default.GetString(buffer, 0, r);
+        if (r > 0)
+        {
+            s = s.Remove(0, s.LastIndexOf(",") + 1).Trim();
+            fs.WriteByte(13);
+            fs.WriteByte(10);
+        }
+        else
+            s = "0";
+        float _totalTime = ((float)(recordDic[name].stopTime - recordDic[name].startTime)) / 10000000;
+        byte[] buffer2 = Encoding.Default.GetBytes(name + "\t,\t" + _totalTime + "\t,\t" + (float.Parse(s) + _totalTime));
+        fs.Write(buffer2, 0, buffer2.Length);
+        fs.Close();
+    }
+}
+public class RecordItem
+{
+    public string filePath;
+    public string name;
+    public long startTime;
+    public long stopTime;
+    public RecordItem()
+    {
+
+    }
+    public RecordItem(string strFilePath, string strName, long numStartTime)
+    {
+        filePath = strFilePath;
+        name = strName;
+        startTime = numStartTime;
+    }
+}
